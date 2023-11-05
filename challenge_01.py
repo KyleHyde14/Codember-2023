0f025ea15e3a916a05007d480401f1d695fa56b0
KyleHyde14 <jesushdezjob@gmail.com>
2023-11-05 13:52:11 +0100

    first challenge completed

--- challenge_01.py
+++ challenge_01.py
@@ -1,1 +1,28 @@
- 
+def decode():
+    message = input('Choose a message to decode: ')
+
+    try:
+        str(message)
+    except:
+        return 'The message must be a string'
+
+    message_cleaned  = message.lower().split()
+
+    dic = {}
+
+    answer = ''
+
+    for _ in message_cleaned:
+        if _ in dic:
+            dic[_] += 1
+        
+        else:
+            dic[_] = 1
+
+    for k,v in dic.items():
+        answer += k + str(v)
+
+    return 'Decoded message is: \n' + answer
+
+if __name__ == '__main__':
+    print(decode())
